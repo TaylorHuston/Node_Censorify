ea928ea4cd6bc88be85dfdd860981efa13941e99
Taylor Huston <taylor.a.huston@gmail.com>
2015-02-14 23:58:35 -0700

    Add censortext.js

---
+++ censortext.js
@@ -0,0 +1,29 @@
+//First Node project
+//From MEAN, chapter 3
+
+var censoredWords["sad", "bad", "mad"];
+var customCensoredWords = []'
+
+function censor(inStr) {
+    for (idx in censoredWords)  {
+        inStr = inStr,replace(sensoredWords[idx], "*****");
+    }
+    
+    for (idx in customCensoredWords) {
+        inStr = inStr.replace(customCensoredWords[idx], "*****");
+    }
+    return inStr;
+}
+
+function addCensoredWord(word) {
+    customCensoredWords.push(word);
+}
+
+function getCensoredWords() {
+    return censoredWords.concat(customCensoredWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords - getCensoredWords;
+
